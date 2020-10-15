c7bc4d9346342c04240179a7725855507d7c6dcc
Gnitch <sujoydcunha@gmail.com>
2020-10-16 01:03:41 +0530

    Updates

---
+++ precache-manifest.caa7f943ffacfb86be57a05d84f12ec7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c360eb182d56e3f035ebf3a196d781",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "47869583bb558c5a2c2e",
+    "url": "/Covid-19-Tracker/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "570ef2e19e27bf244bb0",
+    "url": "/Covid-19-Tracker/static/css/main.aa21fb29.chunk.css"
+  },
+  {
+    "revision": "47869583bb558c5a2c2e",
+    "url": "/Covid-19-Tracker/static/js/2.14b24b1c.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/Covid-19-Tracker/static/js/2.14b24b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570ef2e19e27bf244bb0",
+    "url": "/Covid-19-Tracker/static/js/main.5c39fd34.chunk.js"
+  },
+  {
+    "revision": "3f9bfa923e184b907d1a",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.f79461fb.js"
+  }
+]);
